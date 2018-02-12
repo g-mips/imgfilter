1076e93bd872e0a8670b3fb0b73f1f61a2a2ba82
Grant Merrill <g-mips@users.noreply.github.com>
2018-02-11 17:40:53 -0700

    Refactored jpeg_has_SOI and EOI functions to call a single internal function.

--- src/jpeg.c
+++ src/jpeg.c
@@ -4,6 +4,9 @@
 
 #include "jpeg.h"
 
+static int _jpeg_has_marker(
+    int const MARKER, long const FILE_OFFSET, int const FILE_ORIGIN);
+
 // TODO(g-mips): Maybe expand this to have an ever expanding array of files?
 static FILE *jpeg_file = NULL;
 
@@ -29,19 +32,9 @@ void jpeg_close()
 
 int jpeg_has_SOI()
 {
-    uint8_t buffer[MARK_SIZE];
-    size_t num_read = 0;
-    int error_code = JPEG_SUCCESS;
+    int error_code = _jpeg_has_marker(SOI, 0, SEEK_SET);
 
-    rewind(jpeg_file);
-
-    num_read = fread(buffer, sizeof *buffer, MARK_SIZE, jpeg_file);
-
-    if (num_read != MARK_SIZE)
-    {
-        error_code = JPEG_FILE_READ_ERR;
-    }
-    else if (buffer[0] != BEG_MARK || buffer[1] != SOI)
+    if (error_code == JPEG_INVALID_MARKER_ERR)
     {
         error_code = JPEG_NO_SOI_ERR;
     }
@@ -51,22 +44,47 @@ int jpeg_has_SOI()
 
 int jpeg_has_EOI()
 {
+    int error_code = _jpeg_has_marker(EOI, MARK_SIZE*-1, SEEK_END);
+
+    if (error_code == JPEG_INVALID_MARKER_ERR)
+    {
+        error_code = JPEG_NO_SOI_ERR;
+    }
+
+    return error_code;
+}
+
+static int _jpeg_has_marker(
+    int const MARKER, long const FILE_OFFSET, int const FILE_ORIGIN)
+{
     uint8_t buffer[MARK_SIZE];
     size_t num_read = 0;
     int error_code = JPEG_SUCCESS;
 
-    fseek(jpeg_file, MARK_SIZE*-1, SEEK_END);
+    if (jpeg_file == NULL)
+    {
+        error_code = JPEG_FILE_NOT_OPEN_ERR;
+        goto _jpeg_has_marker_end;
+    }
+
+    // This is done with a seek as well, so in order to reset end-of-file and
+    // error indicators
+    rewind(jpeg_file);
+    fseek(jpeg_file, FILE_OFFSET, FILE_ORIGIN);
 
     num_read = fread(buffer, sizeof *buffer, MARK_SIZE, jpeg_file);
 
     if (num_read != MARK_SIZE)
     {
         error_code = JPEG_FILE_READ_ERR;
-    }
-    else if (buffer[0] != BEG_MARK || buffer[1] != EOI)
-    {
-        error_code = JPEG_NO_EOI_ERR;
+        goto _jpeg_has_marker_end;
     }
 
+    if (buffer[0] != BEG_MARK || buffer[1] != MARKER)
+    {
+        error_code = JPEG_INVALID_MARKER_ERR;
+    }
+
+_jpeg_has_marker_end:
     return error_code;
 }
